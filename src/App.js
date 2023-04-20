013fb4780f7271cadf545658ac4d63ebd60c264a
nicollekoetzler <nickkoetzler@gmail.com>
2023-04-20 16:45:48 -0300

    ESLint config

---
+++ src/App.js
@@ -0,0 +1,9 @@
+/* eslint-disable react/react-in-jsx-scope */
+
+export default function App() {
+    return (
+        <>
+        
+        </>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
-import './assets/styles/reset.css';
-import './assets/styles/style.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+import "./assets/styles/reset.css";
+import "./assets/styles/style.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
