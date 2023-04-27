d03bd745a06e65998b008a165892627f3a5cd2b3
nicollekoetzler <nickkoetzler@gmail.com>
2023-04-27 16:15:02 -0300

    feat: list service's details

--- src/pages/services/details/[id].js
+++ src/pages/services/details/[id].js
@@ -4,38 +4,37 @@ import BottomLayout from "@/layouts/Bottom";
 import Options from "@/layouts/Options";
 import SideBanner from "@/layouts/SideBanner";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+import { getService } from "@/services/servicesRequisitions";
+import ServiceDetail from "@/components/ServiceDetail";
 
 export default function Services() {
   const router = useRouter();
   const { id } = router.query;
   const [service, setService] = useState({});
 
-  /* async function getServiceDetails() {
-    const serviceDetails = await 
-  } */
+  async function getServiceDetails() {
+    try {
+      const serviceDetails = await getService(id);
+      setService(serviceDetails.data);
+      console.log("ALOOOOOO");
+      console.log(serviceDetails);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getServiceDetails();
+  }, [id]);
 
   return (
     <>
       <HeaderLayout />
       <Background>
         <Content>
-          <Info>
-            <h2>
-              Preciso de um profissonal para melhorar meu perfil do linkedin
-            </h2>
-            <h5>Publicado em 18 de Abril</h5>
-            <h3>Descrição do projeto:</h3>
-            <h4>
-              Preciso de um profissonal para melhorar meu perfil do linkedin,
-              tenho um perfil no linkedin e gostaria de fazer melhoria no meu
-              perfil melhorar minhas perfil em geral para que seja mais atrativo
-              <br />
-              Preciso de um profissonal para melhorar meu perfil do linkedin,
-              tenho um perfil no linkedin e gostaria de fazer melhoria no meu
-              perfil melhorar minhas perfil em geral para que seja mais atrativo
-            </h4>
-          </Info>
+          <ServiceDetail service={service} />
           <div>
             <Options />
             <SideBanner />
@@ -51,40 +50,9 @@ const Background = styled.div`
   display: flex;
   align-items: center;
   flex-direction: column;
-`;
+`
 
 const Content = styled.div`
   display: flex;
   margin-top: 64px;
-`;
-
-const Info = styled.div`
-  width: 700px;
-  height: fit-content;
-  background-color: white;
-  border-radius: 12px;
-  border: 1px solid #cdcdcd;
-  padding: 32px;
-  margin-bottom: 16px;
-  margin-right: 32px;
-
-  h2 {
-    font-size: 22px;
-  }
-
-  h3 {
-    font-size: 16px;
-    margin-bottom: 16px;
-    color: #4e693c;
-  }
-
-  h4 {
-    line-height: 128%;
-  }
-
-  h5 {
-    font-size: 14px;
-    color: #727272;
-    margin: 16px 0px 24px 0px;
-  }
-`;
+`
