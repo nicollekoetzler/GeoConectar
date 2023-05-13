150d28708d6fd0edbe65993c1ee7308807b7599b
Gustavo-de-Brito <gustavoaraujo086@gmail.com>
2023-05-13 18:31:36 -0300

    feat: show not logged message if user has no token

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,11 +2,13 @@ import React from "react";
 import "../assets/styles/reset.css";
 import "../assets/styles/style.css";
 import { AppWrapper } from "../../contexts/chatContext";
+import NotLoggedMessage from "@/components/NotLoggedMessage";
 
 // eslint-disable-next-line react/prop-types
 export default function App({ Component, pageProps }) {
   return (
     <AppWrapper>
+        <NotLoggedMessage />
         <Component {...pageProps} />
     </AppWrapper>
   );
