b0ae365d64962800d5360133077e8db97455ac5f
Gustavo-de-Brito <gustavoaraujo086@gmail.com>
2023-07-20 23:09:31 -0300

    style: change document title

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,7 +10,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>GeoConectar</title>
+        <title>GEOCONECTAR</title>
       </Head>
       <AppWrapper>
           <NotLoggedMessage />
