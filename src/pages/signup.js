6712beb672091a83d7e094be19dc3157342c2f84
Gustavo-de-Brito <gustavoaraujo086@gmail.com>
2023-10-24 19:02:19 -0300

    chore: remove unused function

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -13,67 +13,63 @@ export default function SignUp() {
     password: "",
     confirmPassword: "",
   });
-  const [ error, setError ] = useState([]);
+  const [error, setError] = useState([]);
 
   async function handleSubmit(e) {
     e.preventDefault();
     let submitErrors = [];
 
-    
     if (newUser.password !== newUser.confirmPassword) {
-        const DIFFERENT_PASSWORDS_MESSAGE = "As senhas não são iguais";
-        setNewUser({ ...newUser, confirmPassword: ""});
-        submitErrors = [...submitErrors, DIFFERENT_PASSWORDS_MESSAGE];
+      const DIFFERENT_PASSWORDS_MESSAGE = "As senhas não são iguais";
+      setNewUser({ ...newUser, confirmPassword: "" });
+      submitErrors = [...submitErrors, DIFFERENT_PASSWORDS_MESSAGE];
     }
-    if(newUser.cpf.length !== 11) {
-        const CPF_LENGTH_MESSAGE = "O CPF deve conter 11 inteiros";
-        submitErrors = [...submitErrors, CPF_LENGTH_MESSAGE];
+    if (newUser.cpf.length !== 11) {
+      const CPF_LENGTH_MESSAGE = "O CPF deve conter 11 inteiros";
+      submitErrors = [...submitErrors, CPF_LENGTH_MESSAGE];
     }
-    if(newUser.name.length < 3) {
-        const NAME_LENGTH_MESSAGE = "O nome deve conter no mínimo 3 caracteres";
-        submitErrors = [...submitErrors, NAME_LENGTH_MESSAGE];
+    if (newUser.name.length < 3) {
+      const NAME_LENGTH_MESSAGE = "O nome deve conter no mínimo 3 caracteres";
+      submitErrors = [...submitErrors, NAME_LENGTH_MESSAGE];
     }
-    if(newUser.password.length < 6) {
-        const PASSWORD_LENGTH_MESSAGE = "A senha deve conter no mínimo 6 caracteres";
-        submitErrors = [...submitErrors, PASSWORD_LENGTH_MESSAGE];
+    if (newUser.password.length < 6) {
+      const PASSWORD_LENGTH_MESSAGE =
+        "A senha deve conter no mínimo 6 caracteres";
+      submitErrors = [...submitErrors, PASSWORD_LENGTH_MESSAGE];
     }
 
-    if(submitErrors.length > 0) {
-        setError(submitErrors);
-        return;
+    if (submitErrors.length > 0) {
+      setError(submitErrors);
+      return;
     }
-    
+
     try {
-        delete newUser.confirmPassword;
-        await signup(newUser);
-        router.push("/signin");
+      delete newUser.confirmPassword;
+      await signup(newUser);
+      router.push("/signin");
     } catch (err) {
-        const CONFLICT_MESSAGE = "Email ou CPF já cadastrados";
-        
-        if(err.response.status === 409) {
-            setError([ CONFLICT_MESSAGE ]);
+      const CONFLICT_MESSAGE = "Email ou CPF já cadastrados";
+
+      if (err.response.status === 409) {
+        setError([CONFLICT_MESSAGE]);
       }
-      
+
       console.log(err);
     }
   }
-  
+
   function removeErrorMessage(...errorMessages) {
-      let errors = error;
-      
-      for(const errorMessage of errorMessages) {
-        const indexInvalidMessage = errors.indexOf(errorMessage);
-        
-        if(indexInvalidMessage !== -1) {
-            errors = errors.splice(indexInvalidMessage, 1);
-        }
+    let errors = error;
+
+    for (const errorMessage of errorMessages) {
+      const indexInvalidMessage = errors.indexOf(errorMessage);
+
+      if (indexInvalidMessage !== -1) {
+        errors = errors.splice(indexInvalidMessage, 1);
+      }
     }
 
     setError(errors);
-  }
-
-  function removeAllErrorMessage() {
-    setError([]);
   }
 
   function handleCpfChange(e) {
@@ -81,10 +77,10 @@ export default function SignUp() {
     const INVALID_CPF_MESSAGE = "O CPF deve ser composto apenas por números";
     removeErrorMessage(INVALID_CPF_MESSAGE);
 
-    if(cpf.length > 11) return;
-    else if(isNaN(Number(cpf))) {
-        setError([ ...error, INVALID_CPF_MESSAGE ]);
-        return;
+    if (cpf.length > 11) return;
+    else if (isNaN(Number(cpf))) {
+      setError([...error, INVALID_CPF_MESSAGE]);
+      return;
     }
 
     setNewUser({ ...newUser, cpf });
@@ -96,7 +92,7 @@ export default function SignUp() {
         <h1>GeoConectar</h1>
         <h2>Cadastre-se</h2>
         <form onSubmit={handleSubmit}>
-          { error.length > 0 ? <ErrorMessage messages={ error } /> : <></> }
+          {error.length > 0 ? <ErrorMessage messages={error} /> : <></>}
           <h3>Nome completo</h3>
           <FormStyle>
             <input
@@ -125,7 +121,7 @@ export default function SignUp() {
               type="text"
               placeholder="Digite seu CPF (apenas números)"
               value={newUser.cpf}
-              onChange={ handleCpfChange }
+              onChange={handleCpfChange}
               required
             />
           </FormStyle>
@@ -171,7 +167,7 @@ const Background = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color: #F9F9F9;
+  background-color: #f9f9f9;
   font-family: "Roboto";
 
   img {
@@ -197,7 +193,7 @@ const Container = styled.div`
     font-family: "Saira Semi Condensed";
     font-size: 42px;
     font-weight: 700;
-    color: #754D24;
+    color: #754d24;
     text-align: center;
   }
 
@@ -229,20 +225,19 @@ const Container = styled.div`
     padding: 24px 16px;
 
     form {
-        margin: 0 10px; 
+      margin: 0 10px;
     }
 
     h1 {
-        font-size: 36px;
+      font-size: 36px;
     }
 
     h2 {
-        text-align: center;
-        font-size: 20px;
+      text-align: center;
+      font-size: 20px;
     }
   }
 `;
-
 
 const Button = styled.div`
   button {
@@ -261,7 +256,7 @@ const Button = styled.div`
     width: 100%;
 
     button {
-        width: 100%;
+      width: 100%;
     }
   }
 `;
@@ -289,11 +284,11 @@ const FormStyle = styled.div`
     width: 100%;
 
     input {
-        width: 100%;
+      width: 100%;
     }
   }
 `;
 
 const Green = styled.a`
-  color: #754D24;
+  color: #754d24;
 `;
