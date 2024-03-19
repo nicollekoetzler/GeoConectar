656b29b621b490680e3e8ef059784dd7cde22f99
Gustavo de Brito Araújo <gustavoaraujo086@gmail.com>
2024-03-19 14:25:49 -0300

    feat: update strategy tag in the google adsense script

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -15,11 +15,11 @@ export default function App({ Component, pageProps }) {
         <Script
           id="Adsense-id"
           async
-          strategy="afterInteractive"
           onError={(e) => {
             console.error("Script failed to load", e);
           }}
           src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID}`}
+          strategy="lazyOnload"
           crossOrigin="anonymous"
         />
       </Head>
