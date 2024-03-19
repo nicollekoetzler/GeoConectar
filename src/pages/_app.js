ef80c458a303de2636b094ebb1156674edcd9b99
Gustavo de Brito Araújo <gustavoaraujo086@gmail.com>
2024-03-19 13:13:13 -0300

    feat: update google adsense script

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -12,17 +12,17 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <title>GEOCONECTAR</title>
+        <Script
+          id="Adsense-id"
+          async
+          strategy="afterInteractive"
+          onError={(e) => {
+            console.error("Script failed to load", e);
+          }}
+          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID}`}
+          crossOrigin="anonymous"
+        />
       </Head>
-      <Script
-        id="Adsense-id"
-        async
-        strategy="afterInteractive"
-        onError={(e) => {
-          console.error("Script failed to load", e);
-        }}
-        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID}`}
-        crossorigin="anonymous"
-      />
       <AppWrapper>
         <NotLoggedMessage />
         <Component {...pageProps} />
